5eeedbe5c8c6c949e569c30584f7ef659792adb1
martin-bartolo <martin9oh9@gmail.com>
2023-10-23 16:58:21 +0200

    Change pytorch dtype to float16 on cuda devices

--- utils.py
+++ utils.py
@@ -3,10 +3,16 @@ from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 from PIL import Image
 
 model_id = "stabilityai/stable-diffusion-2-1"
-pipe = StableDiffusionPipeline.from_pretrained(model_id)
-pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+    pipe = StableDiffusionPipeline.from_pretrained(
+        model_id, torch_dtype=torch.float16)
+else:
+    device = 'cpu'
+    pipe = StableDiffusionPipeline.from_pretrained(model_id)
+
+pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
 pipe = pipe.to(device)
 
 
